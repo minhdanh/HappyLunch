30a67b8a8b25b71d91075a930ed98fd2f15ca7c0
Linh Nguyen <linhvn09@gmail.com>
2015-12-28 11:26:53 +0700

    add phone and email incase filling the order form

---
+++ db/migrate/20151228042456_add_more_fields_to_user.rb
@@ -0,0 +1,6 @@
+class AddMoreFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :phone_number, :string
+    add_column :users, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228042017) do
+ActiveRecord::Schema.define(version: 20151228042456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20151228042017) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "name"
+    t.string "phone_number"
+    t.string "email"
   end
 
 end
