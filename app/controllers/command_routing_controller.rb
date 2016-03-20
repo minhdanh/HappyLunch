01e1251035d7648cae41662dbff426739b0fc04a
Minh Danh <minhdanh.ngo@gmail.com>
2016-03-20 20:09:54 +0700

    Add orders command

--- app/controllers/command_routing_controller.rb
+++ app/controllers/command_routing_controller.rb
@@ -1,9 +1,23 @@
 class CommandRoutingController < ApplicationController
   def index
     raw_command = params[:text].strip
-    command_params = raw_command.split(/(menu|order) *([\d,\s]+)*/)
+    command_params = raw_command.split(/(menu|orders|order) *([\d,\s]+)*/)
     if command_params[1] == "menu"
       render text: SlackMessageServices::SendMenu.format_content
+    elsif command_params[1] == "orders"
+      text = ""
+      OrderItem.pluck(:dish_id).uniq.each do |dish_id|
+        dish = Dish.find_by(id: dish_id)
+        text << "#{dish.item_number}. #{dish.name} ("
+        order_items = OrderItem.where(dish_id: dish_id)
+        text << order_items.map(&:username).join(", ")
+        text << ")\n"
+      end
+      if text == ""
+        render text: "Nobody ordered yet."
+      else
+        render text: text
+      end
     elsif command_params[1] == "order"
       if command_params[2].present?
           service = SaveOrderItemService.new(params["user_name"], command_params[2])
@@ -15,6 +29,8 @@ class CommandRoutingController < ApplicationController
       else
         render text: "Incorrect syntax"
       end
+    else
+      render text: "Command not recognized."
     end
   end
 end
